c1f65c491219d1b5c3607d19ee2a79bde8fb6a19
Abdul Sabur <alsabur20@gmail.com>
2024-10-20 13:04:55 +0500

    changes

--- Collabrix/Collabrix/Controllers/ProjectController.cs
+++ Collabrix/Collabrix/Controllers/ProjectController.cs
@@ -11,6 +11,51 @@ namespace Collabrix.Controllers
         {
             Configuration = configuration;
         }
+        public async static Task<Project> GetProject(int projectId)
+        {
+            Project? project = null;
+            string? connectionString = Configuration.GetConnectionString("Default");
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    await connection.OpenAsync();
+                    string query = "SELECT * FROM Projects WHERE ProjectId = @ProjectId";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@ProjectId", projectId);
+                        using (SqlDataReader reader = await command.ExecuteReaderAsync()) // Use ExecuteReaderAsync for better async support
+                        {
+                            while (await reader.ReadAsync()) // Use ReadAsync for better async support
+                            {
+                                project = new Project
+                                {
+                                    ProjectId = reader.GetInt32(reader.GetOrdinal("ProjectId")),
+                                    ProjectName = reader.GetString(reader.GetOrdinal("ProjectName")),
+                                    Description = reader.GetString(reader.GetOrdinal("Description")),
+                                    StartDate = reader.GetDateTime(reader.GetOrdinal("StartDate")),
+                                    EndDate = reader.GetDateTime(reader.GetOrdinal("EndDate")),
+                                    ProjectType = reader.GetInt32(reader.GetOrdinal("ProjectTypeLookupId")),
+                                    CreatedBy = reader.GetInt32(reader.GetOrdinal("CreatedBy")),
+                                    CreatedAt = reader.GetDateTime(reader.GetOrdinal("CreatedAt")),
+                                    UpdatedAt = reader.GetDateTime(reader.GetOrdinal("UpdatedAt")),
+                                    IsDeleted = reader.GetInt32(reader.GetOrdinal("IsDeleted"))
+                                };
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message + ex.StackTrace);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+            return project; // Moved return statement outside the try block
+        }
 
         public async static Task<List<Project>> GetProjects(int userId)
         {
