fd755fb05d6d64108d171c06816b469c33fddaa1
Abdul Sabur <alsabur20@gmail.com>
2024-10-20 21:24:04 +0500

    maryam ko bhej rha

--- Collabrix/Collabrix/Controllers/LookUpcontroller.cs
+++ Collabrix/Collabrix/Controllers/LookUpcontroller.cs
@@ -13,7 +13,7 @@ namespace Collabrix.Controllers
         }
 
         public async static Task<string> getValueById(int id)
-        { 
+        {
             string value = "";
             string connectionString = Configuration.GetConnectionString("Default");
             using (SqlConnection connection = new SqlConnection(connectionString))
@@ -24,9 +24,9 @@ namespace Collabrix.Controllers
                     string query = $"SELECT Value FROM LookUp WHERE LookupId = {id}";
                     using (SqlCommand command = new SqlCommand(query, connection))
                     {
-                        using (SqlDataReader reader =  command.ExecuteReader())
+                        using (SqlDataReader reader = command.ExecuteReader())
                         {
-                            while ( reader.Read())
+                            while (reader.Read())
                             {
                                 value = reader.GetString(reader.GetOrdinal("Value"));
                             }
@@ -46,7 +46,7 @@ namespace Collabrix.Controllers
         }
 
         public static async Task<int> getIdByValue(string value)
-        { 
+        {
             int id = 0;
             string connectionString = Configuration.GetConnectionString("Default");
             using (SqlConnection connection = new SqlConnection(connectionString))
@@ -57,9 +57,9 @@ namespace Collabrix.Controllers
                     string query = $"SELECT LookupId FROM LookUp WHERE Value = '{value}'";
                     using (SqlCommand command = new SqlCommand(query, connection))
                     {
-                        using (SqlDataReader reader =   command.ExecuteReader())
+                        using (SqlDataReader reader = command.ExecuteReader())
                         {
-                            while ( reader.Read())
+                            while (reader.Read())
                             {
                                 id = reader.GetInt32(reader.GetOrdinal("LookupId"));
                             }
@@ -123,9 +123,9 @@ namespace Collabrix.Controllers
                     string query = $"SELECT Value FROM LookUp WHERE Category = 'ProjectType'";
                     using (SqlCommand command = new SqlCommand(query, connection))
                     {
-                        using (SqlDataReader reader =  command.ExecuteReader())
+                        using (SqlDataReader reader = command.ExecuteReader())
                         {
-                            while ( reader.Read())
+                            while (reader.Read())
                             {
                                 projectTypes.Add(reader.GetString(reader.GetOrdinal("Value")));
                             }
@@ -144,5 +144,44 @@ namespace Collabrix.Controllers
             return projectTypes;
         }
 
+        public static async Task<List<Lookup>> GetLookupsByategory(string category)
+        {
+            List<Lookup> lookups = new List<Lookup>();
+            string connectionString = Configuration.GetConnectionString("Default");
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    await connection.OpenAsync();
+                    string query = "SELECT * FROM LookUp WHERE Category = @category";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@category", category);
+                        using (SqlDataReader reader = await command.ExecuteReaderAsync())
+                        {
+                            while (await reader.ReadAsync())
+                            {
+                                Lookup lookup = new Lookup
+                                {
+                                    LookupId = reader.GetInt32(reader.GetOrdinal("LookupId")),
+                                    Category = reader.GetString(reader.GetOrdinal("Category")),
+                                    Value = reader.GetString(reader.GetOrdinal("Value"))
+                                };
+                                lookups.Add(lookup);
+                            }
+                            return await Task.FromResult(lookups);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message + ex.StackTrace);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+        }
     }
 }
--- Collabrix/Collabrix/Pages/ProjectPages/createProject.cshtml.cs
+++ Collabrix/Collabrix/Pages/ProjectPages/createProject.cshtml.cs
@@ -2,88 +2,88 @@ using Collabrix.Controllers;
 using Collabrix.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.Collections.Generic;
 
 
 namespace Collabrix.Pages
 {
     public class createProjectModel : PageModel
     {
+        private int _userId;
+        // For Creation
         [BindProperty]
         public Project Project { get; set; }
+
+        // For Displaying
+        public List<Lookup>? Statuses { get; private set; }
+        public List<Lookup>? ProjectTypes { get; private set; }
+        public List<Lookup>? Roles { get; private set; }
+        public List<User>? Users { get; private set; }
+
+        public string? stageName { get; set; }
         [BindProperty]
-        public string Status { get; set; }
+        public string? stageDescription { get; set; }
         [BindProperty]
-        public string ProjectType { get; set; }
-        [BindProperty]
-        public List<string> Statuses {  get; set; } 
-        [BindProperty]
-        public List<string> ProjectTypes { get; set; }
-        [BindProperty]
-        public string stageName { get; set; }
-        [BindProperty]
-        public string stageDescription { get; set; }
-        [BindProperty]
-        public Dictionary<string, int> ProjectStages { get; set; }
+        public Dictionary<string, int>? ProjectStages { get; set; }
 
 
-        public async void OnGet()
+        public async void OnGet(int userId)
         {
             try
             {
-                Statuses = await LookUpcontroller.getStatuses();
-                Statuses.Insert(0, "Select Project Status");
-                ProjectTypes = await LookUpcontroller.getProjectTypes();
-                ProjectTypes.Insert(0, "Select Project Type");
+                _userId = 1;
+                Statuses = await LookUpcontroller.GetLookupsByategory("ProjectStatus");
+                ProjectTypes = await LookUpcontroller.GetLookupsByategory("ProjectType");
+                Roles = await LookUpcontroller.GetLookupsByategory("Role");
+                Users = await UserController.GetUsers();
             }
-            catch
+            catch (Exception ex)
             {
-
+                TempData["ErrorOnServer"] = ex.Message + ex.StackTrace;
             }
 
         }
 
-        public async void OnPostCreateProjectAsync()
+        public async Task<IActionResult> OnPostCreateProjectAsync()
         {
             try
-            { 
-                Project.ProjectType = await LookUpcontroller.getIdByValue(ProjectType);
-                Project.Status = await LookUpcontroller.getIdByValue(Status);
-                Project.CreatedBy = 1;
-                int projectId =  await ProjectController.CreateProject(Project);
-                if(ProjectStages != null) {
-                    foreach (var item in ProjectStages)
-                    {
-                        ProjectTaskStage projectTaskStage = new ProjectTaskStage
-                        {
-                            ProjectId = projectId,
-                            StageName = item.Key,
-                            StageDescription = item.Value.ToString(),
-                            CreatedBy = 1
-                        };
-                        await ProjectTaskStageController.CreateProjectTaskStage(projectTaskStage);
-                    }
-                }
+            {
+                Project.CreatedBy = _userId;
+                Project.CreatedAt = DateTime.Now;
+                //int projectId = await ProjectController.CreateProject(Project);
+                //if (ProjectStages != null)
+                //{
+                //    foreach (var item in ProjectStages)
+                //    {
+                //        ProjectTaskStage projectTaskStage = new ProjectTaskStage
+                //        {
+                //            ProjectId = projectId,
+                //            StageName = item.Key,
+                //            StageDescription = item.Value.ToString(),
+                //            CreatedBy = 1
+                //        };
+                //        await ProjectTaskStageController.CreateProjectTaskStage(projectTaskStage);
+                //    }
+                //}
             }
             catch (Exception ex)
             {
                 throw new Exception(ex.Message + ex.StackTrace);
             }
-
+            return RedirectToPage("/ProjectPages/createProject");
         }
 
         public async void OnPostCreateStageAsync()
         {
             try
             {
-                ProjectTaskStage projectTaskStage = new ProjectTaskStage
-                {
-                    ProjectId = 1,
-                    StageName = stageName,
-                    StageDescription = stageDescription,
-                    CreatedBy = 1
-                };
-                await ProjectTaskStageController.CreateProjectTaskStage(projectTaskStage);
+                //ProjectTaskStage projectTaskStage = new ProjectTaskStage
+                //{
+                //    ProjectId = 1,
+                //    StageName = stageName,
+                //    StageDescription = stageDescription,
+                //    CreatedBy = 1
+                //};
+                //await ProjectTaskStageController.CreateProjectTaskStage(projectTaskStage);
             }
             catch (Exception ex)
             {
@@ -91,4 +91,4 @@ namespace Collabrix.Pages
             }
         }
     }
-}   
+}
