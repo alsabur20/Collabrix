b906e8ba5451b3cc3e8dd30bfe0c562516dc5ab8
Abdul Sabur <alsabur20@gmail.com>
2024-10-22 22:03:36 +0500

    manually changin with main

--- Collabrix/Collabrix/Controllers/LookUpcontroller.cs
+++ Collabrix/Collabrix/Controllers/LookUpcontroller.cs
@@ -1,4 +1,5 @@
-﻿using Collabrix.Models;
+﻿
+using Collabrix.Models;
 using System.Data.SqlClient;
 using System.Net.NetworkInformation;
 
@@ -12,48 +13,8 @@ namespace Collabrix.Controllers
             Configuration = configuration;
         }
 
-        public async static Task<List<Lookup>> GetLookUps(int lookUpType)
+        public async static Task<string> getValueById(int id)
         {
-            List<Lookup> lookUps = new List<Lookup>();
-            string connectionString = Configuration.GetConnectionString("Default");
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                try
-                {
-                    await connection.OpenAsync();
-                    string query = $"SELECT * FROM LookUp WHERE LookUpType = {lookUpType}";
-                    using (SqlCommand command = new SqlCommand(query, connection))
-                    {
-                        using (SqlDataReader reader = await command.ExecuteReaderAsync())
-                        {
-                            while (await reader.ReadAsync())
-                            {
-                                Lookup lookUp = new Lookup
-                                {
-                                    LookupId = reader.GetInt32(reader.GetOrdinal("LookUpId")),
-                                    Value = reader.GetString(reader.GetOrdinal("Value")),
-                                    Category = reader.GetString(reader.GetOrdinal("Category")),
-                                    Description = reader.GetString(reader.GetOrdinal("Description"))
-                                };
-                                lookUps.Add(lookUp);
-                            }
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    throw new Exception(ex.Message + ex.StackTrace);
-                }
-                finally
-                {
-                    connection.Close();
-                }
-            }
-            return lookUps;
-        }
-
-        public static async Task<string> getValueById(int id)
-        { 
             string value = "";
             string connectionString = Configuration.GetConnectionString("Default");
             using (SqlConnection connection = new SqlConnection(connectionString))
@@ -64,7 +25,7 @@ namespace Collabrix.Controllers
                     string query = $"SELECT Value FROM LookUp WHERE LookupId = {id}";
                     using (SqlCommand command = new SqlCommand(query, connection))
                     {
-                        using (SqlDataReader reader =  command.ExecuteReader())
+                        using (SqlDataReader reader = command.ExecuteReader())
                         {
                             while (reader.Read())
                             {
@@ -85,18 +46,19 @@ namespace Collabrix.Controllers
             return value;
         }
 
-        public static int getIdByValue(string value)
-        { 
+        public static async Task<int> getIdByValue(string value)
+        {
             int id = 0;
             string connectionString = Configuration.GetConnectionString("Default");
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 try
                 {
+                    await connection.OpenAsync();
                     string query = $"SELECT LookupId FROM LookUp WHERE Value = '{value}'";
                     using (SqlCommand command = new SqlCommand(query, connection))
                     {
-                        using (SqlDataReader reader =  command.ExecuteReader())
+                        using (SqlDataReader reader = command.ExecuteReader())
                         {
                             while (reader.Read())
                             {
@@ -129,9 +91,9 @@ namespace Collabrix.Controllers
                     string query = $"SELECT Value FROM LookUp WHERE Category = 'Status'";
                     using (SqlCommand command = new SqlCommand(query, connection))
                     {
-                        using (SqlDataReader reader = await command.ExecuteReaderAsync())
+                        using (SqlDataReader reader = command.ExecuteReader())
                         {
-                            while (await reader.ReadAsync())
+                            while (reader.Read())
                             {
                                 statuses.Add(reader.GetString(reader.GetOrdinal("Value")));
                             }
@@ -162,9 +124,9 @@ namespace Collabrix.Controllers
                     string query = $"SELECT Value FROM LookUp WHERE Category = 'ProjectType'";
                     using (SqlCommand command = new SqlCommand(query, connection))
                     {
-                        using (SqlDataReader reader = await command.ExecuteReaderAsync())
+                        using (SqlDataReader reader = command.ExecuteReader())
                         {
-                            while (await reader.ReadAsync())
+                            while (reader.Read())
                             {
                                 projectTypes.Add(reader.GetString(reader.GetOrdinal("Value")));
                             }
@@ -183,5 +145,44 @@ namespace Collabrix.Controllers
             return projectTypes;
         }
 
+        public static async Task<List<Lookup>> GetLookupsByategory(string category)
+        {
+            List<Lookup> lookups = new List<Lookup>();
+            string connectionString = Configuration.GetConnectionString("Default");
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    await connection.OpenAsync();
+                    string query = "SELECT * FROM LookUp WHERE Category = @category";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@category", category);
+                        using (SqlDataReader reader = await command.ExecuteReaderAsync())
+                        {
+                            while (await reader.ReadAsync())
+                            {
+                                Lookup lookup = new Lookup
+                                {
+                                    LookupId = reader.GetInt32(reader.GetOrdinal("LookupId")),
+                                    Category = reader.GetString(reader.GetOrdinal("Category")),
+                                    Value = reader.GetString(reader.GetOrdinal("Value"))
+                                };
+                                lookups.Add(lookup);
+                            }
+                            return await Task.FromResult(lookups);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message + ex.StackTrace);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+        }
     }
 }
